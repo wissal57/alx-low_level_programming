8cdc91a52b4d07d33b0ebaf214cf9c290fd3678a
wissal57 <ahajamwissal@gmail.com>
2023-08-31 04:28:52 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,10 +9,10 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-    if (index >= sizeof(unsigned long int) * 8)
-        return -1;
+	if (index > 63)
+		return (-1);
 
-    *n = (*n) & ~(1UL << index);
-    return 1;
+	*n = (~(1UL << index) & *n);
+	return (1);
 }
 
