d9cf3864ab15bb0a197cd07724e61118a412a51d
wissal57 <ahajamwissal@gmail.com>
2023-08-31 04:22:40 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,23 +8,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int result = 0;
+	int i;
+	unsigned int dec_val = 0;
 
-    if (b == NULL)
-        return 0;
+	if (!b)
+		return (0);
 
-    for (int i = 0; b[i] != '\0'; i++)
-    {
-        if (b[i] == '0' || b[i] == '1')
-        {
-            result = result * 2 + (b[i] - '0');
-        }
-        else
-        {
-            return 0;
-        }
-    }
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
+	}
 
-    return result;
+	return (dec_val);
 }
 
